1a5b2365ee1d1d12fc27b4fd91482c3a73977a6b
Kelley Carr <kelley@hapnyn.com>
2017-03-12 20:58:50 -0700

    Plot 4, code and graph

---
+++ plot4.R
@@ -0,0 +1,57 @@
+plot4 <- function() {
+  if (!exists("household_power_consumption")) {
+    if (!exists("read_format_data")) {
+      source("read_format_data.R")
+    }
+    household_power_consumption <- read_format_data()
+  }
+  
+  png(filename = "plot4.png")
+  par(mfrow = c(2,2))
+  
+  ## Plot 1 of 4
+  plot(household_power_consumption$DateTime,
+       household_power_consumption$Global_active_power, 
+       ylab = "Global Active Power",
+       xlab = "",
+       type = "l")
+  
+  ## Plot 2 of 4
+  plot(household_power_consumption$DateTime,
+       household_power_consumption$Voltage,
+       xlab = "datetime",
+       ylab = "Volgate",
+       type = "l")
+  
+  ## Plot 3 of 4
+  plot(household_power_consumption$DateTime,
+       household_power_consumption$Sub_metering_1, 
+       ylab = "Energy sub metering",
+       xlab = "",
+       type = "n")
+  points(household_power_consumption$DateTime,
+         household_power_consumption$Sub_metering_1,
+         type = "l",
+         col = "black")
+  points(household_power_consumption$DateTime,
+         household_power_consumption$Sub_metering_2,
+         type = "l",
+         col = "red")
+  points(household_power_consumption$DateTime,
+         household_power_consumption$Sub_metering_3,
+         type = "l",
+         col = "blue")  
+  legend("topright",
+         c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
+         col = c("black", "red", "blue"), 
+         lty = c(1, 1, 1),
+         bty = "n")
+  
+  ## Plot 4 of 4
+  plot(household_power_consumption$DateTime,
+       household_power_consumption$Global_reactive_power,
+       xlab = "datetime",
+       ylab = "Global_reactive_power",
+       type = "l")
+  dev.off()
+}
